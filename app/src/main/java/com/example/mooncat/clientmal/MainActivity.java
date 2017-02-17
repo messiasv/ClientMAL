1df8708cd7124080688875ecc87cd4a9c1528432
Valentin <mv77270@gmail.com>
2017-02-17 17:52:03 +0100

    Replace Objects.equals replace Objects.equals(A a, A b) with a.equals(b)

--- app/src/main/java/com/example/mooncat/clientmal/MainActivity.java
+++ app/src/main/java/com/example/mooncat/clientmal/MainActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.util.Objects;
-
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
@@ -16,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String username = Tools.readFile(getApplicationContext(), "username");
         String credentials = Tools.readFile(getApplicationContext() ,"creds");
-        if (Objects.equals(username, "") || Objects.equals(credentials, "")) {
+        if (username.equals("") || credentials.equals("")) {
             Intent intent = new Intent(getApplicationContext(), UserLoginActivity.class);
             startActivity(intent);
             finish();
